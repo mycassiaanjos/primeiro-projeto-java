d8f4174bbc35fa83fa2ddbb25b0882cb36d2bd70
Mycássia <mycacaline@gmail.com>
2023-03-27 17:41:39 -0300

    feat: adição e impressão de variável no terminal

--- primeiro-projeto/src/main/java/com/example/App.java
+++ primeiro-projeto/src/main/java/com/example/App.java
@@ -4,10 +4,11 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Olá,");
+        var nome = "Mycássia Caline dos Anjos Pessoa";
+        System.out.println(nome);
+
     }
 }
